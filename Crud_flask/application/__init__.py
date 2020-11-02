fd1b904bf833de582855017e8211f512cb735f53
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-02 15:52:02 +0000

    database task 3 flasky boy

---
+++ Crud_flask/application/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:dev@ops@35.246.125.154/flask_db"
+
+db = SQLAlchemy(app)
+
+from application import routes
