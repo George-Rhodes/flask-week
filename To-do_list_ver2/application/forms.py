c0303a0ad1dfbf882900040f34b43bc15c0f64ec
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-05 14:33:09 +0000

    few more changes done

--- To-do_list_ver2/application/forms.py
+++ To-do_list_ver2/application/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+
+
+class TodoCheck:
+	def __init__(self, message):
+		self.message = message
+
+	def __call__(self, form, field):
+		todoList = ToDoList.query.all()
+		for todo in todoList:
+			if todo.task == field.data:
+				raise ValidationError(self.message)
 
 class TodoForm(FlaskForm):
 	task = StringField('Task',
-		validators=[DataRequired()])
+		validators=[DataRequired(),
+		TodoCheck(message='Task already exists')])
 
 	submit = SubmitField('Add Todo')
