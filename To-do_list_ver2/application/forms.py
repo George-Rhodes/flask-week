9a249d2820dd8c13614e0608d683321bb37af5ad
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-05 15:08:08 +0000

    updataed add

--- To-do_list_ver2/application/forms.py
+++ To-do_list_ver2/application/forms.py
@@ -1,6 +1,7 @@
-from flask_wtf import FlaskForm
+from flask_wtf import dbFlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
+from application.models import ToDoList
 
 
 
